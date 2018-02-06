bf6f4a04deb74ba44e6d35d754419f15288f8fe9
Justin Casey <jcasey@localhost.localdomain>
2018-02-05 22:16:39 -0500

    Second COmmit

--- app/src/main/java/com/jcaseydev/bakingapp/Model/Recipe.java
+++ app/src/main/java/com/jcaseydev/bakingapp/Model/Recipe.java
@@ -3,6 +3,9 @@ package com.jcaseydev.bakingapp.Model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jcasey on 2/5/18.
  */
@@ -11,6 +14,8 @@ public class Recipe implements Parcelable{
 
     private String name;
     private String servings;
+    private List<Ingredient> ingredients;
+    private List<Step> steps;
 
     public String getName() {
         return name;
@@ -18,6 +23,14 @@ public class Recipe implements Parcelable{
 
     public String getServings() {
         return servings;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
     }
 
     @Override
@@ -29,11 +42,19 @@ public class Recipe implements Parcelable{
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
         dest.writeString(servings);
+        dest.writeList(ingredients);
+        dest.writeList(steps);
     }
 
     private Recipe(Parcel in) {
         name = in.readString();
         servings = in.readString();
+
+        ingredients = new ArrayList<>();
+        in.readList(ingredients, Ingredient.class.getClassLoader());
+
+        steps = new ArrayList<>();
+        in.readList(steps, Step.class.getClassLoader());
     }
 
     public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
