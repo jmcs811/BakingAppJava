258542d465c799d0e9339ad2ccb4b58fe8df661d
Justin Casey <jcasey@localhost.localdomain>
2018-02-06 20:47:48 -0500

    ...

--- app/src/main/java/com/jcaseydev/bakingapp/MainFragment.java
+++ app/src/main/java/com/jcaseydev/bakingapp/MainFragment.java
@@ -54,11 +54,12 @@ public class MainFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
         recipeListView = v.findViewById(R.id.recipe_recycler_view);
-        layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         recipeListView.setLayoutManager(layoutManager);
 
         adapter = new MyAdapter(recipesList);
         recipeListView.setAdapter(adapter);
+
         return v;
     }
 
@@ -72,6 +73,74 @@ public class MainFragment extends Fragment {
 
     private class GetData extends AsyncTask<String, Void, Recipe[]> {
 
+        private Recipe[] getJsonData(String json) throws JSONException{
+
+            final String TAG_ID = "id";
+            final String TAG_NAME = "name";
+            final String TAG_INGREDIENTS = "ingredients";
+            final String TAG_QUANTITY = "quantity";
+            final String TAG_MEASURE = "measure";
+            final String TAG_INGREDIENT = "ingredient";
+            final String TAG_STEPS = "steps";
+            final String TAG_SDESCP = "shortDescription";
+            final String TAG_DESCP = "description";
+            final String TAG_VURL = "videoURL";
+            final String TAG_SERVINGS = "servings";
+
+            JSONArray recipeArray = new JSONArray(json);
+
+            Recipe[] recipes = new Recipe[recipeArray.length()];
+
+            for (int i = 0; i < recipeArray.length(); i++) {
+                recipes[i] = new Recipe();
+
+                JSONObject recipeInfo = recipeArray.getJSONObject(i);
+
+                recipes[i].setName(recipeInfo.getString(TAG_NAME));
+                recipes[i].setServings(recipeInfo.getString(TAG_SERVINGS));
+
+                List<Ingredient> ingredientList = new ArrayList<>();
+                List<Step> stepList = new ArrayList<>();
+
+                JSONArray ingredientArray = new JSONArray(recipeInfo.getString(TAG_INGREDIENTS));
+                Ingredient[] ingredients = new Ingredient[ingredientArray.length()];
+
+                for(int j = 0; j < ingredientArray.length(); j++){
+                    ingredients[j] = new Ingredient();
+
+                    JSONObject ingredientInfo = ingredientArray.getJSONObject(j);
+
+                    ingredients[j].setQuantity(ingredientInfo.getInt(TAG_QUANTITY));
+                    ingredients[j].setMeasure(ingredientInfo.getString(TAG_MEASURE));
+                    ingredients[j].setIngredient(ingredientInfo.getString(TAG_INGREDIENT));
+
+                    ingredientList.add(ingredients[j]);
+                }
+
+                recipes[i].setIngredients(ingredientList);
+
+                JSONArray stepArray = new JSONArray(recipeInfo.getString(TAG_STEPS));
+                Step[] steps = new Step[stepArray.length()];
+
+                for(int k = 0; k < stepArray.length(); k++) {
+                    steps[k] = new Step();
+
+                    JSONObject stepInfo = stepArray.getJSONObject(k);
+
+                    steps[k].setShortDescription(stepInfo.getString(TAG_SDESCP));
+                    steps[k].setDescription(stepInfo.getString(TAG_DESCP));
+                    steps[k].setId(stepInfo.getInt(TAG_ID));
+                    steps[k].setVideoUrl(stepInfo.getString(TAG_VURL));
+
+                    stepList.add(steps[k]);
+                }
+
+                recipes[i].setSteps(stepList);
+            }
+            return recipes;
+        }
+
+
      @Override
      protected Recipe[] doInBackground(String... urls) {
          String url = urls[0];
@@ -80,7 +149,8 @@ public class MainFragment extends Fragment {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        Response response = null;
+
+        Response response;
 
         String jsonData = null;
 
@@ -99,89 +169,17 @@ public class MainFragment extends Fragment {
          return null;
      }
 
-     private Recipe[] getJsonData(String json) throws JSONException{
-
-         final String TAG_ID = "id";
-         final String TAG_NAME = "name";
-         final String TAG_INGREDIENTS = "ingredients";
-         final String TAG_QUANTITY = "quantity";
-         final String TAG_MEASURE = "measure";
-         final String TAG_INGREDIENT = "ingredient";
-         final String TAG_STEPS = "steps";
-         final String TAG_SDESCP = "shortDescription";
-         final String TAG_DESCP = "description";
-         final String TAG_VURL = "videoURL";
-         final String TAG_SERVINGS = "servings";
-
-         JSONArray recipeArray = new JSONArray(json);
-
-         Recipe[] recipes = new Recipe[recipeArray.length()];
-
-         for (int i = 0; i < recipeArray.length(); i++) {
-             recipes[i] = new Recipe();
-
-             JSONObject recipeInfo = recipeArray.getJSONObject(i);
-
-             recipes[i].setName(recipeInfo.getString(TAG_NAME));
-             recipes[i].setServings(recipeInfo.getString(TAG_SERVINGS));
-
-             List<Ingredient> ingredientList = new ArrayList<>();
-             List<Step> stepList = new ArrayList<>();
-
-             JSONArray ingredientArray = new JSONArray(recipeInfo.getString(TAG_INGREDIENTS));
-             Ingredient[] ingredients = new Ingredient[ingredientArray.length()];
-
-             for(int j = 0; j < ingredientArray.length(); j++){
-                 ingredients[j] = new Ingredient();
-
-                 JSONObject ingredientInfo = ingredientArray.getJSONObject(j);
-
-                 ingredients[j].setQuantity(ingredientInfo.getInt(TAG_QUANTITY));
-                 ingredients[j].setMeasure(ingredientInfo.getString(TAG_MEASURE));
-                 ingredients[j].setIngredient(ingredientInfo.getString(TAG_INGREDIENT));
-
-                 ingredientList.add(ingredients[j]);
-             }
-
-             recipes[i].setIngredients(ingredientList);
-
-             JSONArray stepArray = new JSONArray(recipeInfo.getString(TAG_STEPS));
-             Step[] steps = new Step[stepArray.length()];
-
-             for(int k = 0; k < stepArray.length(); k++) {
-                 steps[k] = new Step();
-
-                 JSONObject stepInfo = stepArray.getJSONObject(k);
-
-                 steps[k].setShortDescription(stepInfo.getString(TAG_SDESCP));
-                 steps[k].setDescription(stepInfo.getString(TAG_DESCP));
-                 steps[k].setId(stepInfo.getInt(TAG_ID));
-                 steps[k].setVideoUrl(stepInfo.getString(TAG_VURL));
-
-                 stepList.add(steps[k]);
-             }
-
-             recipes[i].setSteps(stepList);
-         }
-         return recipes;
-     }
-
         @Override
         protected void onPostExecute(Recipe[] recipes) {
             super.onPostExecute(recipes);
             if(recipes != null) {
                 recipesList.addAll(Arrays.asList(recipes));
-                //Toast.makeText(getContext(), "list filled", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(getContext(), "SORRY NO DATA", Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
             }
         }
     }
 
-
-
     private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-
         private List<Recipe> dataSet;
 
         class ViewHolder extends RecyclerView.ViewHolder{
@@ -193,21 +191,20 @@ public class MainFragment extends Fragment {
             }
         }
 
-        MyAdapter(List<Recipe> recipes){
+        MyAdapter(List<Recipe> recipes) {
             dataSet = recipes;
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
-
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
             return new ViewHolder(view);
         }
 
-        @Override
         public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
             holder.recipeName.setText(dataSet.get(position).getName());
         }
+
 
         @Override
         public int getItemCount() {
